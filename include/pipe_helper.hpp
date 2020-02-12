78ab27dd53599894171f6e46c63f0e68758668b7
Sindre Stephansen <sindre@sindrestephansen.com>
2020-02-12 11:28:49 +0100

    Prepare for windows support

--- include/pipe_helper.hpp
+++ include/pipe_helper.hpp
@@ -2,7 +2,12 @@
 #define PIPE_HELPER_H
 
 #include <string>
+
+#ifdef __unix__
 #include <fstream>
+#else
+#include <windows.h>
+#endif
 
 namespace sandbox {
     class Pipe {
@@ -11,22 +16,34 @@ namespace sandbox {
 
 	template<typename T>
 	T read() {
+	    #ifdef __unix__
 	    stream.open(path, std::ios::in);
 	    char data;
 	    stream >> data;
 	    stream.close();
 	    return data;
+	    #else
+
+	    #endif
 	}
 
 	template<typename T>
 	void write(T data) {
+	    #ifdef __unix__
 	    stream.open(path, std::ios::out);
 	    stream << data << std::endl;
 	    stream.close();
+	    #else
+
+	    #endif
 	}
     private:
 	std::string path;
+	#ifdef __unix__
 	std::fstream stream;
+	#else
+
+	#endif
     };
 }
 
--- src/pipe_helper.cpp
+++ src/pipe_helper.cpp
@@ -6,7 +6,11 @@
 namespace sandbox {
     Pipe::Pipe(std::string name)
     {
+	#ifdef __unix__
 	path = "/tmp/" + name;
 	mkfifo(path.c_str(), 0666);
+	#else
+
+	#endif
     }
 }
